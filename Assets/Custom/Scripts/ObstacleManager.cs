d1096df179173e0e1b02458f895ae0c8b1f0acdf
Luciano <loliveir@sfu.ca>
2021-03-25 16:18:55 -0700

    obstacles closer to walls

--- Assets/Custom/Scripts/ObstacleManager.cs
+++ Assets/Custom/Scripts/ObstacleManager.cs
@@ -27,12 +27,19 @@ public class ObstacleManager : MonoBehaviour
         GameObject obstacleInstance;
         Vector3 obstaclePosition;
         int randomIndex;
+        float positionX, positionZ;
+        float safeRegion = 3;
 
         for (int obstacleIndex = 0; obstacleIndex < obstacleCount; obstacleIndex++)
         {
             randomIndex = Random.Range(0, obstacles.Length);
             obstacleInstance = GameObject.Instantiate(obstacles[randomIndex]);
-            obstaclePosition = new Vector3(Random.Range(obstacleMinPos.x, obstacleMaxPos.x), obstacleInstance.transform.position.y, Random.Range(obstacleMinPos.z, obstacleMaxPos.z));
+            do {
+		positionX = Random.Range(obstacleMinPos.x, obstacleMaxPos.x);
+		positionZ = Random.Range(obstacleMinPos.z, obstacleMaxPos.z);
+            } while ((positionX < obstacleMinPos.x + safeRegion && positionZ < obstacleMinPos.z + safeRegion)
+            	  || (positionX > obstacleMaxPos.x - safeRegion && positionZ > obstacleMaxPos.z - safeRegion));
+            obstaclePosition = new Vector3(positionX, obstacleInstance.transform.position.y, positionZ);
             obstacleInstance.transform.position = obstaclePosition;
         }
     }

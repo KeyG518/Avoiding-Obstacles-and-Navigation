e0402196f02417cbc7d2acb617c086d8b978690b
Luciano <loliveir@sfu.ca>
2021-03-25 16:30:34 -0700

    generating people away from robot's and goal's positions

--- Assets/Custom/Scripts/PersonManager.cs
+++ Assets/Custom/Scripts/PersonManager.cs
@@ -28,12 +28,19 @@ public class PersonManager : MonoBehaviour
         GameObject personInstance;
         PersonController personController;
         Vector3 personPosition;
+        float positionX, positionZ;
+        float safeRegion = 3;
 
         for (int personIndex = 0; personIndex < personCount; personIndex++)
         {
             personInstance = GameObject.Instantiate(person);
             personController = personInstance.GetComponent<PersonController>();
-            personPosition = new Vector3(Random.Range(personMinPos.x, personMaxPos.x), personInstance.transform.position.y, Random.Range(personMinPos.z, personMaxPos.z));
+            do {
+		positionX = Random.Range(personMinPos.x, personMaxPos.x);
+		positionZ = Random.Range(personMinPos.z, personMaxPos.z);
+            } while ((positionX < personMinPos.x + safeRegion && positionZ < personMinPos.z + safeRegion)
+            	  || (positionX > personMaxPos.x - safeRegion && positionZ > personMaxPos.z - safeRegion));
+            personPosition = new Vector3(positionX, personInstance.transform.position.y, positionZ);
 
             personController.setTransformPosition(personPosition);
             personController.setSpeed(Random.Range(personController.speed - personSpeedVariation, personController.speed + personSpeedVariation));

81d2294ba4f52c0e914ca8381db26cce5e1aaee4
Alex Volkanov <avolkano@sfu.ca>
2021-04-11 23:10:56 -0700

    Refactor Code

--- Assets/Custom/Scripts/ObstacleManager.cs
+++ Assets/Custom/Scripts/ObstacleManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
+    public GameObject player, finalGoal;
     public GameObject[] obstacles;
     public Vector3 obstacleMinPos;
     public Vector3 obstacleMaxPos;
@@ -28,19 +29,26 @@ public class ObstacleManager : MonoBehaviour
         Vector3 obstaclePosition;
         int randomIndex;
         float positionX, positionZ;
-        float safeRegion = 3;
 
         for (int obstacleIndex = 0; obstacleIndex < obstacleCount; obstacleIndex++)
         {
             randomIndex = Random.Range(0, obstacles.Length);
             obstacleInstance = GameObject.Instantiate(obstacles[randomIndex]);
+
             do {
-		positionX = Random.Range(obstacleMinPos.x, obstacleMaxPos.x);
-		positionZ = Random.Range(obstacleMinPos.z, obstacleMaxPos.z);
-            } while ((positionX < obstacleMinPos.x + safeRegion && positionZ < obstacleMinPos.z + safeRegion)
-            	  || (positionX > obstacleMaxPos.x - safeRegion && positionZ > obstacleMaxPos.z - safeRegion));
-            obstaclePosition = new Vector3(positionX, obstacleInstance.transform.position.y, positionZ);
+                positionX = Random.Range(obstacleMinPos.x, obstacleMaxPos.x);
+                positionZ = Random.Range(obstacleMinPos.z, obstacleMaxPos.z);
+                obstaclePosition = new Vector3(positionX, obstacleInstance.transform.position.y, positionZ);
+            }
+            while (!isPositionValid(obstaclePosition, 1.0f));
+
             obstacleInstance.transform.position = obstaclePosition;
         }
+    }
+
+
+    private bool isPositionValid(Vector3 position, float distanceThreshold)
+    {
+        return Vector3.Distance(position, player.transform.position) > distanceThreshold && Vector3.Distance(position, finalGoal.transform.position) > distanceThreshold;
     }
 }
--- Assets/Custom/Scripts/PersonManager.cs
+++ Assets/Custom/Scripts/PersonManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PersonManager : MonoBehaviour
 {
-    public GameObject person;
+    public GameObject player, finalGoal, person;
     public Vector3 personMinPos;
     public Vector3 personMaxPos;
     public int personCount = 10;
@@ -29,21 +29,27 @@ public class PersonManager : MonoBehaviour
         PersonController personController;
         Vector3 personPosition;
         float positionX, positionZ;
-        float safeRegion = 3;
 
         for (int personIndex = 0; personIndex < personCount; personIndex++)
         {
             personInstance = GameObject.Instantiate(person);
             personController = personInstance.GetComponent<PersonController>();
+
             do {
-		positionX = Random.Range(personMinPos.x, personMaxPos.x);
-		positionZ = Random.Range(personMinPos.z, personMaxPos.z);
-            } while ((positionX < personMinPos.x + safeRegion && positionZ < personMinPos.z + safeRegion)
-            	  || (positionX > personMaxPos.x - safeRegion && positionZ > personMaxPos.z - safeRegion));
-            personPosition = new Vector3(positionX, personInstance.transform.position.y, positionZ);
+                positionX = Random.Range(personMinPos.x, personMaxPos.x);
+                positionZ = Random.Range(personMinPos.z, personMaxPos.z);
+                personPosition = new Vector3(positionX, personInstance.transform.position.y, positionZ);
+            }
+            while (!isPositionValid(personPosition, 1.0f));
 
             personController.setTransformPosition(personPosition);
             personController.setSpeed(Random.Range(personController.speed - personSpeedVariation, personController.speed + personSpeedVariation));
         }
+    }
+
+
+    private bool isPositionValid(Vector3 position, float distanceThreshold)
+    {
+        return Vector3.Distance(position, player.transform.position) > distanceThreshold && Vector3.Distance(position, finalGoal.transform.position) > distanceThreshold;
     }
 }
